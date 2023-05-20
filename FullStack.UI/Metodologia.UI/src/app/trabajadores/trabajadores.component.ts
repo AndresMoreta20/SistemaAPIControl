769dfbc092663fa67145c0acf2c437dd9fc9354e
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-05-19 20:31:33 -0500

    Tabla final

--- FullStack.UI/Metodologia.UI/src/app/trabajadores/trabajadores.component.ts
+++ FullStack.UI/Metodologia.UI/src/app/trabajadores/trabajadores.component.ts
@@ -8,7 +8,7 @@ import { SharedService } from 'src/app/shared.service';
   styleUrls: ['./trabajadores.component.css']
 })
 export class TrabajadoresComponent {
-  arregloObjetos: any;
+  arregloObjetos: any = [];
   res: any;
   resultado: any;
   emisorCodigo: string = "";
@@ -16,37 +16,33 @@ export class TrabajadoresComponent {
   constructor(private http: HttpClient, private sharedService: SharedService) { }
 
 
-  data = [
-    { campo: 'COMP_Codigo', valor: 2 },
-    { campo: 'Id_Trabajador', valor: 1 },
-    { campo: 'Tipo_trabajador', valor: 'Administrativo' },
-    // Resto de los campos y valores...
-  ];
 
   
   ngOnInit(): void {
 
     this.emisorCodigo = this.sharedService.emisorCodigo;
-    console.log(this.emisorCodigo);
-   
+  
+   this.onSearch();
 
     
   }
 
   onButton(){
-    console.log(this.emisorCodigo[2]);
-    this.onSearch();
+    //this.onSearch();
+    console.log(this.arregloObjetos);
   }
 
   onSearch() {
     const url = `https://localhost:7036/api/GestionUsuario/Trabajador/Search?sucursal=${this.emisorCodigo}`;
   
     this.http.get(url).subscribe(async (response) => {
+    //  console.log(response);
       this.res = response;
-      this.res = this.res[0];
-      this.resultado = this.res[0];
-      console.log(this.res);
-      this.arregloObjetos = this.res;
+     this.arregloObjetos = this.res;
+    // this.res = this.res[0];
+    //  this.resultado = this.res[0];
+     // console.log(this.res);
+    // this.arregloObjetos = this.res;
   
       // Ejemplo de uso del resultado
      
